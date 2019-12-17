98ec994d8d932cee677aafcff3e1d6b412e98b9f
dongdong <1075073101@qq.com>
2019-12-17 20:35:49 +0800

    mythreadpool

--- src/main/java/ThreadPoolExecutor/MyThreadPoolExecutor.java
+++ src/main/java/ThreadPoolExecutor/MyThreadPoolExecutor.java
@@ -1,7 +1,28 @@
 package ThreadPoolExecutor;
 
+import java.util.concurrent.*;
+
+
 public class MyThreadPoolExecutor {
     public static void main(String[] args) {
 
+        ExecutorService threadPool=new ThreadPoolExecutor(
+                2,
+                5,
+                1L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+        try {
+            for (int i = 0; i < 8; i++) {
+                final int tempI = i;
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "受理" + "客户" + tempI);
+                });
+            }
+        }finally {
+            threadPool.shutdown();
+        }
     }
 }
